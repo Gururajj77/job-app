1553708684e2e5d79ad6b42024b42553c6bfa615
Gururajj77 <gururajtakeshi@gmail.com>
2023-03-20 23:19:40 +0530

    popular jobs code have been written

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
-import styles from './popularjobcard.style'
+import styles from "./popularjobcard.style";
 
-const PopularJobCard = () => {
+const PopularJobCard = ({item, selectedJob, handlePressCard}) => {
+
+  
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    
+    <TouchableOpacity
+      style={styles.container(selectedJob, item)}
+      onPress={()=> handlePressCard(item)}
+    >
+      <TouchableOpacity
+        style={styles.logoContainer(selectedJob, item)}
+      >
+        <Image
+          source={{uri: item.employer_logo}}
+          resizeMethod="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>{item.employer_name}</Text>
+      <View>
+        <Text style={styles.jobName(selectedJob, item)} numberOfLines={1}>
+          {item.job_title}
+        </Text>
+        <Text style={styles.location}>{item.job_country}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
